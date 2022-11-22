94766871e7dae19438c50262fe8080e85d467c43
Luizinho079 <115711699+Luizinho079@users.noreply.github.com>
2022-11-22 10:08:03 -0300

    Add files via upload

---
+++ JURIDICO-LUIZ CARLOS/Processos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juridico.models
+{
+    class Processos
+    {
+        public int Numero { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public pessoa Solicitante { get; set; }
+
+        public pessoa acusado { get; set; }
+
+        public Pedidos pedido { get; set; }
+
+        public double getValortotalPedidos()
+        {
+            return 1;
+        }
+
+        public double getQuantidadePedidos()
+        {
+            return 1;
+        }
+        public List<Processo> getprocessosPorEscreito()
+        {
+            return 1;
+        }
+    }
+    
+    
+}
